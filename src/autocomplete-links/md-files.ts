b094793f8d2f533e6818afc451cce1974c0458cb
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-11 08:51:11 -0500

    polish

--- src/autocomplete-links/md-files.ts
+++ src/autocomplete-links/md-files.ts
@@ -3,11 +3,10 @@ import * as path from "path"
 
 const IGNORE = [".git", "node_modules", "vendor"]
 
-// mdFiles provides the relative path of all Markdown files in the given root folder
+/** mdFiles provides the relative path of all Markdown files in the given root folder */
 export async function mdFiles(root: string, subFolder = ""): Promise<string[]> {
-  const result: string[] = []
   // NOTE: this method scans subfolders concurrently to reduce latency of the interactive autocomplete dialog.
-  // This array contains the promises of those ongoing subfolder scans.
+  const result: string[] = []
   const folderPromises: Array<Promise<string[]>> = []
   for (const file of await fs.readdir(path.join(root, subFolder))) {
     if (file.startsWith(".") || IGNORE.includes(file)) {
