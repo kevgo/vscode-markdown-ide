11599ae8617cfcf66886bc4d0627a1c617e39326
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-11 09:20:38 -0500

    Documentation tests (#8)

---
+++ text-run/make-target.js
@@ -0,0 +1,38 @@
+const fs = require("fs").promises
+const os = require("os")
+const path = require("path")
+
+module.exports = async function (activity) {
+  const have = activity.nodes.text().replace("make", "").trim()
+  activity.name(`make target "${have}" exists`)
+  const want = await existingMakeTasks(activity)
+  if (!want.includes(have)) {
+    throw new Error(`Make target "${have}" not found in [${want.join(", ")}]`)
+  }
+}
+
+// existingMakeTasks provides all tasks in the Makefile in the current folder.
+async function existingMakeTasks(activity) {
+  const makefilePath = path.join(activity.configuration.sourceDir, "Makefile")
+  const makefileContent = await fs.readFile(makefilePath, "utf8")
+  const results = []
+  for (const line of makefileContent.split(os.EOL)) {
+    if (lineDefinesMakeCommand(line)) {
+      results.push(extractMakeCommand(line))
+    }
+  }
+  return results
+}
+
+// returns whether the given line from a Makefile
+// defines a Make command
+function lineDefinesMakeCommand(line) {
+  return makeCommandRE.test(line) && !line.startsWith(".PHONY")
+}
+const makeCommandRE = /^[^ ]+:/
+
+// returns the defined command name
+// from a Makefile line that defines a Make command
+function extractMakeCommand(line) {
+  return line.split(":")[0]
+}
