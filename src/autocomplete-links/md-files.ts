937908a438accd08d7c746150f5dbdd9fec63b6d
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-11 08:35:16 -0500

    polish

--- src/autocomplete-links/md-files.ts
+++ src/autocomplete-links/md-files.ts
@@ -1,14 +1,14 @@
 import { promises as fs } from "fs"
 import * as path from "path"
 
-const ignore = [".git", "node_modules", "vendor"]
+const IGNORE = [".git", "node_modules", "vendor"]
 
 // getFiles provides
 export async function mdFiles(root: string, subFolder = ""): Promise<string[]> {
   const result: string[] = []
   const folderPromises: Array<Promise<string[]>> = []
   for (const file of await fs.readdir(path.join(root, subFolder))) {
-    if (file.startsWith(".") || ignore.includes(file)) {
+    if (file.startsWith(".") || IGNORE.includes(file)) {
       continue
     }
     const relFilePath = path.join(subFolder, file)
