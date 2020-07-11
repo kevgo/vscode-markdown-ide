6c1862bce53cbb59836ab0e01795f2e5eac3ec9b
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-11 08:47:53 -0500

    doc

--- src/autocomplete-links/md-files.ts
+++ src/autocomplete-links/md-files.ts
@@ -6,6 +6,8 @@ const IGNORE = [".git", "node_modules", "vendor"]
 // mdFiles provides the relative path of all Markdown files in the given root folder
 export async function mdFiles(root: string, subFolder = ""): Promise<string[]> {
   const result: string[] = []
+  // NOTE: as a performance optimization, this method scans subfolders concurrently.
+  // This array contains the promises of those ongoing subfolder scans.
   const folderPromises: Array<Promise<string[]>> = []
   for (const file of await fs.readdir(path.join(root, subFolder))) {
     if (file.startsWith(".") || IGNORE.includes(file)) {
