9a0cc75642089b55d93939990fc6b1038f13aecc
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-11 08:11:03 -0500

    cleanup

--- src/autocomplete-links/md-files.ts
+++ src/autocomplete-links/md-files.ts
@@ -5,7 +5,7 @@ const ignore = [".git", "node_modules", "vendor"]
 
 // getFiles provides
 export async function mdFiles(root: string, subFolder = ""): Promise<string[]> {
-  let result: string[] = []
+  const result: string[] = []
   for (const file of await fs.readdir(path.join(root, subFolder))) {
     if (file.startsWith(".") || ignore.includes(file)) {
       continue
@@ -14,7 +14,8 @@ export async function mdFiles(root: string, subFolder = ""): Promise<string[]> {
     const absFilePath = path.join(root, relFilePath)
     const fileInfo = await fs.stat(absFilePath)
     if (fileInfo.isDirectory()) {
-      result = result.concat(await mdFiles(root, relFilePath))
+      const files = await mdFiles(root, relFilePath)
+      result.push(...files)
     } else {
       if (file.endsWith(".md")) {
         result.push(relFilePath)
