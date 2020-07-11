c44aa1aa322ac067fce4a0a6dcfcdd0224649ea0
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-11 08:42:43 -0500

    Improve test

--- src/autocomplete-links/md-files.test.ts
+++ src/autocomplete-links/md-files.test.ts
@@ -4,7 +4,7 @@ import { strict as assert } from "assert"
 test("mdFiles", async function () {
   const have = await mdFiles(".")
   assert.deepEqual(
-    ["DEVELOPMENT.md", "README.md", "documentation/test.md"],
+    ["DEVELOPMENT.md", "README.md", "documentation/fixtures/test.md"],
     have
   )
 })
