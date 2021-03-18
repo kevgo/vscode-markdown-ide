7a86cb3344c86af59a119c8b94c7c21b67e02bad
Kevin Goslar <kevin.goslar@gmail.com>
2021-03-17 19:18:39 -0500

    progress

--- src/autocomplete-links/make-links.ts
+++ src/autocomplete-links/make-links.ts
@@ -1,5 +1,6 @@
 import { promises as fs } from "fs"
 import { posix as path } from "path"
+import * as vscode from "vscode"
 
 import { firstLine } from "../helpers/first-line"
 import { removeLeadingPounds } from "../helpers/remove-leading-pounds"
@@ -11,16 +12,20 @@ export async function makeMdLinks(
   allFiles: string[]
 ): Promise<string[]> {
   const result: string[] = []
+  const config = vscode.workspace.getConfiguration("markdownIDE")
+  const reS = config.get<string>("autocompleteTitleRegex") ?? "^#+ (.*)$"
+  const titleRE = new RegExp(reS)
+
   function linkToFile(filePath: string, content: string): void {
     const relativeFile = path.relative(path.dirname(document), filePath)
-    result.push(makeMdLink(relativeFile, content))
+    result.push(makeMdLink(relativeFile, content, titleRE))
   }
-  const promises = allFiles
+  const operations = allFiles
     .map((filename) => path.join(dir, filename))
     .map((filePath) =>
       fs.readFile(filePath, "utf-8").then(linkToFile.bind(null, filePath))
     )
-  await Promise.all(promises)
+  await Promise.all(operations)
   return result
 }
 
@@ -39,9 +44,20 @@ export function makeImgLinks(
   return result
 }
 
-export function makeMdLink(fileName: string, fileContent: string): string {
-  const title = removeLink(removeLeadingPounds(firstLine(fileContent)))
-  return `[${title}](${fileName})`
+export function makeMdLink(
+  fileName: string,
+  fileContent: string,
+  titleRE: RegExp
+): string {
+  const titleLine = firstLine(fileContent)
+  const match = titleRE.exec(titleLine)
+  let title = ""
+  if (match != null && match.length > 1) {
+    title = match[1]
+  } else {
+    title = removeLeadingPounds(titleLine)
+  }
+  return `[${removeLink(title)}](${fileName})`
 }
 
 export function makeImgLink(fileName: string): string {
