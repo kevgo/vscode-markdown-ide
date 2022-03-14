5577674514b3516b8f88f7f0668bf98f7840f5fc
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-14 16:38:12 -0500

    links.ts

--- src/helpers/links.ts
+++ src/helpers/links.ts
@@ -12,21 +12,26 @@ const titleRE = /\[([^\]]*)\]/
 
 /** removes all links to the given target from the given text */
 export function removeWithTarget(args: { target: string; text: string }): string {
-  return args.text.replace(new RegExp(`\\[(.*?)\\]\\(${args.target}\\)`, "g"), "$1")
+  return args.text.replace(
+    new RegExp(`\\[(.*?)\\]\\(${args.target}\\)`, "g"),
+    "$1"
+  )
 }
 
 /** replaces the given oldTarget in all Markdown links of the given text with the newTarget */
 export function replaceTarget(args: { newTarget: string; oldTarget: string; text: string }): string {
-  const regex = new RegExp(`\\[(.*?)\\]\\(${args.oldTarget}\\)`, "g")
-  const replacement = `[$1](${args.newTarget})`
-  return args.text.replace(regex, replacement)
+  return args.text.replace(
+    new RegExp(`\\[(.*?)\\]\\(${args.oldTarget}\\)`, "g"),
+    `[$1](${args.newTarget})`
+  )
 }
 
 /** replaces the given oldTitle in all links with the given oldTitle and target in the given text with the given newTitle */
 export function replaceTitle(
   args: { newTitle: string; oldTitle: string; target: string; text: string }
 ): string {
-  const re = new RegExp(`\\[${args.oldTitle}\\]\\(${args.target}\\)`, "g")
-  const replacement = `[${args.newTitle}](${args.target})`
-  return args.text.replace(re, replacement)
+  return args.text.replace(
+    new RegExp(`\\[${args.oldTitle}\\]\\(${args.target}\\)`, "g"),
+    `[${args.newTitle}](${args.target})`
+  )
 }
