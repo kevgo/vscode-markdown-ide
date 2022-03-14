44de8d08efab1e65e8a810a5bc85c6caba5ff8fe
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-14 13:59:33 -0500

    img-files.ts

--- src/autocomplete-links/img-files.ts
+++ src/autocomplete-links/img-files.ts
@@ -4,11 +4,5 @@ import * as files from "../helpers/files"
 
 /** provides the filenames of all images in the given directory */
 export async function imgFiles(dir: string): Promise<string[]> {
-  const result: string[] = []
-  for (const file of await fs.readdir(dir)) {
-    if (files.isImage(file)) {
-      result.push(file)
-    }
-  }
-  return result
+  return (await fs.readdir(dir)).filter(files.isImage)
 }
