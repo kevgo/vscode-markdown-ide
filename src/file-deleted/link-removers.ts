5cecdcc5d9ad5f59d338f51d874ceb848643dcc0
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-14 12:52:29 -0500

    Optimize LinkRemover

--- src/file-deleted/link-removers.ts
+++ src/file-deleted/link-removers.ts
@@ -1,25 +1,15 @@
-/** describes a replacement of a regex with given text */
-interface Replacement {
-  readonly regex: RegExp
-  readonly text: string
-}
-
 /**
  * replaces occurrences of the given filenames inside Markdown links to the given new filenames.
  * Call `register` to define the replacements, then `process` to replace them in a file content.
  */
 export class LinkRemovers {
-  private readonly replacements: Replacement[]
-
-  constructor() {
-    this.replacements = []
-  }
+  private readonly replacements: { readonly regex: RegExp; readonly text: string }[] = []
 
   // registers a new replacement from old to new
   register(fileName: string): void {
     this.replacements.push({
       regex: new RegExp(`\\[(.*?)\\]\\(${fileName}\\)`, "g"),
-      text: `$1`
+      text: "$1"
     })
   }
 
