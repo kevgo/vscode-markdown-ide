0276c20e78c1b631cf8dc9cef8c89214e13dbf74
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-14 13:01:24 -0500

    Optimize helper functions

--- src/helpers/first-line.test.ts
+++ src/helpers/first-line.test.ts
@@ -3,7 +3,13 @@ import { strict as assert } from "assert"
 import { firstLine } from "./first-line"
 
 suite("firstLine", function() {
+  test("single-line string", function() {
+    assert.equal(firstLine("# Foo"), "# Foo")
+  })
   test("multi-line string", function() {
     assert.equal(firstLine("# Foo\nbar"), "# Foo")
+  })
+  test("empty string", function() {
+    assert.equal(firstLine(""), "")
   })
 })
--- src/helpers/is-image-file.ts
+++ src/helpers/is-image-file.ts
@@ -2,10 +2,5 @@ const imgExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tif", "tiff"]
 
 /** indicates whether the given filename belongs to an image file */
 export function isImageFile(fileName: string): boolean {
-  for (const ext of imgExtensions) {
-    if (fileName.endsWith(ext)) {
-      return true
-    }
-  }
-  return false
+  return imgExtensions.some((ext) => fileName.endsWith(ext))
 }
