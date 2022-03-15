7fd0b9da012ccd24ee9b55e7aeba2afefcedd9c0
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-15 11:05:13 -0500

    line.ts

--- src/helpers/line.ts
+++ src/helpers/line.ts
@@ -8,7 +8,7 @@ const newLineRE = /\n/g
 
 /** provides the first line of the given text */
 export function first(text: string): string {
-  // NOTE: implemented as a for low-level loop for performance reasons
+  // NOTE: implemented as a low-level loop for performance reasons
   let i = 0
   while (text[i] !== "\n" && i < text.length) {
     i++
@@ -18,7 +18,7 @@ export function first(text: string): string {
 
 /** provides the content of the given line with leading # removed */
 export function removeLeadingPounds(line: string): string {
-  // NOTE: implemented as for low-level loops for performance reasons
+  // NOTE: implemented as low-level loops for performance reasons
   let i = 0
   while (line[i] === "#" && i < line.length) {
     i++
