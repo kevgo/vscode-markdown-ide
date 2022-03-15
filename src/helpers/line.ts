91b6024c7198e5cc9b46e8df30d918e0eb95a9d0
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-15 10:59:35 -0500

    Performance optimization for helpers

--- src/helpers/line.ts
+++ src/helpers/line.ts
@@ -8,14 +8,23 @@ const newLineRE = /\n/g
 
 /** provides the first line of the given text */
 export function first(text: string): string {
-  return text.split("\n", 1)[0]
+  // NOTE: implemented as a for low-level loop for performance reasons
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == "\n") {
+      return text.substring(0, i)
+    }
+  }
+  return text
 }
 
 /** provides the content of the given line with leading # removed */
 export function removeLeadingPounds(line: string): string {
-  let i = 0
-  while (line[i] === "#") {
-    i++
+  // NOTE: implemented as a for low-level loop for performance reasons
+  for (let i = 0; i < line.length; i++) {
+    const c = line[i]
+    if (c !== "#" && c !== " ") {
+      return line.substring(i)
+    }
   }
-  return line.substr(i).trim()
+  return ""
 }
