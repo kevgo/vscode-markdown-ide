6936c127c91083835706918f1617bff989c48651
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-26 15:23:53 -0500

    file-saved.ts

--- src/file-saved/file-saved.ts
+++ src/file-saved/file-saved.ts
@@ -5,27 +5,12 @@ import * as tikibase from "./tikibase"
 
 /** provides a callback function to provide to vscode.workspace.onDidSaveTextDocument */
 export function createCallback(args: { debug: vscode.OutputChannel; workspacePath: string }): () => void {
-  const manager = new SaveEventManager(args)
-  return manager.fileSaved.bind(manager)
-}
-
-/** collection of state and logic around handling onDidSaveTextDocument events in VSCode */
-class SaveEventManager {
-  readonly collection: vscode.DiagnosticCollection
-  readonly debug: vscode.OutputChannel
-  readonly workspacePath: string
-
-  constructor(args: { debug: vscode.OutputChannel; workspacePath: string }) {
-    this.collection = vscode.languages.createDiagnosticCollection("Markdown IDE")
-    this.debug = args.debug
-    this.workspacePath = args.workspacePath
-  }
-
-  async fileSaved() {
-    const errorMessages = await tikibase.run({ debug: this.debug, execOpts: { cwd: this.workspacePath } })
-    this.collection.clear()
+  const collection = vscode.languages.createDiagnosticCollection("Markdown IDE")
+  return async function() {
+    const errorMessages = await tikibase.run({ debug: args.debug, execOpts: { cwd: args.workspacePath } })
+    collection.clear()
     groupByFile(errorMessages).forEach((messagesForFile, file) => {
-      const uri = vscode.Uri.file(path.join(this.workspacePath, file))
+      const uri = vscode.Uri.file(path.join(args.workspacePath, file))
       const diagnostics: vscode.Diagnostic[] = messagesForFile.map((message: tikibase.Message) => {
         return {
           range: new vscode.Range(message.line, message.start, message.line, message.end),
@@ -33,7 +18,7 @@ class SaveEventManager {
           severity: vscode.DiagnosticSeverity.Error
         }
       })
-      this.collection.set(uri, diagnostics)
+      collection.set(uri, diagnostics)
     })
   }
 }
