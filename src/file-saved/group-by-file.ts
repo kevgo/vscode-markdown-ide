6a58f0ebf1ee3679932bff4f1e4cf2619f68b7eb
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-26 14:52:52 -0500

    group-by-file.ts

--- src/file-saved/group-by-file.ts
+++ src/file-saved/group-by-file.ts
@@ -1,5 +1,6 @@
 import { Message } from "./tikibase"
 
+/** groups the given messages by file */
 export function groupByFile(messages: Message[]): Map<string, Message[]> {
   const result: Map<string, Message[]> = new Map()
   for (const message of messages) {
