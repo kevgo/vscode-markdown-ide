5acdff5d557a61a7a931babfdc1693be6c0b2307
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-28 12:56:04 -0500

    headings.ts

--- src/helpers/headings.ts
+++ src/helpers/headings.ts
@@ -1,7 +1,10 @@
 /** provides all headings in the given file content */
 export function inFile(text: string, headings: Set<string>) {
   const matches = text.matchAll(headingRE)
-  for (const match of matches || []) {
+  if (!matches) {
+    return
+  }
+  for (const match of matches) {
     headings.add(match[1])
   }
 }
