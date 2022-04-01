38e83ebd7f6f80ba5ab5d261dbf163efb5e8bbab
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-31 21:13:40 -0500

    follow-bidi-link.test.ts

--- src/follow-link/follow-bidi-link.test.ts
+++ src/follow-link/follow-bidi-link.test.ts
@@ -6,11 +6,12 @@ import { extractLinkTarget, isWebLink, locateLinkWithTarget } from "./follow-bid
 suite("followBiDiLink", function() {
     test("extractLinkTarget", function() {
         const give = "one [title1](target1.md) two [title2](target2.md) three";
-        for (let i = 0; i < 29; i++) {
+        const link2start = 29;
+        for (let i = 0; i < link2start; i++) {
             const have = extractLinkTarget(give, i);
             assert.equal(have, "target1.md", `pos ${i} -> ${have}`);
         }
-        for (let i = 37; i < give.length; i++) {
+        for (let i = link2start; i < give.length; i++) {
             const have = extractLinkTarget(give, i);
             assert.equal(have, "target2.md", `pos ${i} -> ${have}`);
         }
