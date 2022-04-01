3470c2d6c30cdaf50f82d4ea14f780c2ee74dc6e
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-31 21:08:08 -0500

    follow-bidi-link.test.ts follow-bidi-link.ts

--- src/follow-link/follow-bidi-link.test.ts
+++ src/follow-link/follow-bidi-link.test.ts
@@ -1,40 +1,40 @@
-import { strict as assert } from "assert"
-import * as vscode from "vscode"
+import { strict as assert } from "assert";
+import * as vscode from "vscode";
 
-import { extractLinkTarget, isWebLink, locateLinkWithTarget } from "./follow-bidi-link"
+import { extractLinkTarget, isWebLink, locateLinkWithTarget } from "./follow-bidi-link";
 
-suite("followLinks", function() {
-  test("extractLinkTarget", function() {
-    const give = "one [title1](target1.md) two [title2](target2.md) three"
-    for (let i = 0; i < 37; i++) {
-      const have = extractLinkTarget(give, i)
-      assert.equal(have, "target1.md", `pos ${i} -> ${have}`)
-    }
-    for (let i = 37; i < give.length; i++) {
-      const have = extractLinkTarget(give, i)
-      assert.equal(have, "target2.md", `pos ${i} -> ${have}`)
-    }
-  })
-  test("isWebLink", function() {
-    const tests = {
-      "http://acme.com": true,
-      "https://acme.com": true,
-      "filename.md": false,
-      "httpfile.md": false,
-      "httpsfile.md": false
-    }
-    for (const [give, want] of Object.entries(tests)) {
-      const have = isWebLink(give)
-      assert.equal(have, want, `${give} --> ${have}`)
-    }
-  })
-  test("locatePhraseInText", function() {
-    const give = `# title
+suite("followBiDiLinks", function() {
+    test("extractLinkTarget", function() {
+        const give = "one [title1](target1.md) two [title2](target2.md) three";
+        for (let i = 0; i < 29; i++) {
+            const have = extractLinkTarget(give, i);
+            assert.equal(have, "target1.md", `pos ${i} -> ${have}`);
+        }
+        for (let i = 37; i < give.length; i++) {
+            const have = extractLinkTarget(give, i);
+            assert.equal(have, "target2.md", `pos ${i} -> ${have}`);
+        }
+    });
+    test("isWebLink", function() {
+        const tests = {
+            "http://acme.com": true,
+            "https://acme.com": true,
+            "filename.md": false,
+            "httpfile.md": false,
+            "httpsfile.md": false,
+        };
+        for (const [give, want] of Object.entries(tests)) {
+            const have = isWebLink(give);
+            assert.equal(have, want, `${give} --> ${have}`);
+        }
+    });
+    test("locateLinkWithTarget", function() {
+        const give = `# title
 text
 one [link](target.md) two
-three`
-    const want = new vscode.Range(2, 4, 2, 21)
-    const have = locateLinkWithTarget({ target: "target.md", text: give })
-    assert.deepEqual(have, want)
-  })
-})
+three`;
+        const want = new vscode.Position(2, 4);
+        const have = locateLinkWithTarget({ target: "target.md", text: give });
+        assert.deepEqual(have, want);
+    });
+});
--- src/follow-link/follow-bidi-link.ts
+++ src/follow-link/follow-bidi-link.ts
@@ -1,100 +1,108 @@
-import * as path from "path"
-import * as vscode from "vscode"
+import * as path from "path";
+import * as vscode from "vscode";
 
 export async function followBiDiLink(): Promise<void> {
-  const oldDocument = vscode.window.activeTextEditor?.document
-  if (!oldDocument) {
-    return
-  }
-  const oldFilePath = oldDocument.fileName
-  const oldCursor = vscode.window.activeTextEditor?.selection.start
-  if (!oldCursor) {
-    return
-  }
-  const cursorLine = oldDocument.lineAt(oldCursor.line)
-  const linkTarget = extractLinkTarget(cursorLine.text, oldCursor.character)
-  if (!linkTarget) {
-    return
-  }
-  if (isWebLink(linkTarget)) {
-    await openWebLink(linkTarget)
-    return
-  }
-  const oldFileName = path.basename(oldFilePath)
-  const newPath = path.resolve(path.dirname(oldFilePath), linkTarget)
-  const newFileContent = await openFileLink(newPath)
-  if (!newFileContent) {
-    return
-  }
-  const newCursor = locateLinkWithTarget({ target: oldFileName, text: newFileContent })
-  if (!newCursor) {
-    return
-  }
-  const editor = vscode.window.activeTextEditor
-  if (!editor) {
-    return
-  }
-  editor.selection = new vscode.Selection(newCursor, newCursor)
-  vscode.window.activeTextEditor?.revealRange(editor.selection)
+    const oldDocument = vscode.window.activeTextEditor?.document;
+    if (!oldDocument) {
+        return;
+    }
+    const oldFilePath = oldDocument.fileName;
+    const oldCursor = vscode.window.activeTextEditor?.selection.start;
+    if (!oldCursor) {
+        return;
+    }
+    const cursorLine = oldDocument.lineAt(oldCursor.line);
+    const linkTarget = extractLinkTarget(cursorLine.text, oldCursor.character);
+    if (!linkTarget) {
+        return;
+    }
+    if (isWebLink(linkTarget)) {
+        await openWebLink(linkTarget);
+        return;
+    }
+    const oldFileName = path.basename(oldFilePath);
+    const newPath = path.resolve(path.dirname(oldFilePath), linkTarget);
+    const newFileContent = await openFileLink(newPath);
+    if (!newFileContent) {
+        return;
+    }
+    const newCursor = locateLinkWithTarget({ target: oldFileName, text: newFileContent });
+    if (!newCursor) {
+        return;
+    }
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+    editor.selection = new vscode.Selection(newCursor, newCursor);
+    vscode.window.activeTextEditor?.revealRange(editor.selection);
 }
 
 /** provides the range where the first link with the given target occurs in the given text */
 export function locateLinkWithTarget(
-  args: { target: string; text: string }
+    args: { target: string; text: string },
 ): vscode.Position | undefined {
-  const re = new RegExp(`\\[[^\\]]*\\]\\(${args.target}\\)`)
-  for (const [i, line] of args.text.split(/\r?\n/).entries()) {
-    const match = re.exec(line)
-    if (!match) {
-      continue
+    const re = new RegExp(`\\[[^\\]]*\\]\\(${args.target}\\)`);
+    for (const [i, line] of args.text.split(/\r?\n/).entries()) {
+        const match = re.exec(line);
+        if (!match) {
+            continue;
+        }
+        return new vscode.Position(i, match.index);
     }
-    return new vscode.Position(i, match.index)
-  }
 }
 
 /** opens the given link in the default web browser */
 async function openWebLink(link: string) {
-  await vscode.env.openExternal(vscode.Uri.parse(link))
+    await vscode.env.openExternal(vscode.Uri.parse(link));
 }
 
 /** opens a new tab with the given document */
 async function openFileLink(link: string): Promise<string | null> {
-  const doc = await vscode.workspace.openTextDocument(vscode.Uri.file(link))
-  await vscode.window.showTextDocument(doc)
-  return doc.getText()
+    const doc = await vscode.workspace.openTextDocument(vscode.Uri.file(link));
+    await vscode.window.showTextDocument(doc);
+    return doc.getText();
 }
 
 /** provides the target of the Markdown link around the given cursor position in the given text */
 export function extractLinkTarget(lineText: string, cursorColumn: number): string | undefined {
-  // go left until we find `(`
-  let start = cursorColumn
-  while (start >= 0 && lineText[start] !== "(") {
-    start--
-  }
-  // if we didn't find it, go right until we find `(`
-  if (start === -1) {
-    start = cursorColumn
-    while (start <= lineText.length && lineText[start] !== "(") {
-      start++
+    // go left until we find `[`
+    let start = cursorColumn;
+    while (start >= 0 && lineText[start] !== "[") {
+        start--;
     }
-  }
-  if (start === lineText.length + 1) {
-    // start token not found in entire string
-    return undefined
-  }
-  // go right until we find `)`
-  let end = start
-  while (end <= lineText.length && lineText[end] !== ")") {
-    end++
-  }
-  if (end === lineText.length + 1) {
-    // end token not found in entire string
-    return undefined
-  }
-  return lineText.substring(start + 1, end)
+    // if we didn't find it, go right until we find `[`
+    if (start === -1) {
+        start = cursorColumn;
+        while (start <= lineText.length && lineText[start] !== "[") {
+            start++;
+        }
+    }
+    if (start === lineText.length + 1) {
+        void vscode.window.showErrorMessage("No link found");
+        return;
+    }
+    // go right until we find `(`
+    while (start <= lineText.length && lineText[start] !== "(") {
+        start++;
+    }
+    if (start === lineText.length + 1) {
+        void vscode.window.showErrorMessage("No link found");
+        return;
+    }
+    // go right until we find `)`
+    let end = start;
+    while (end <= lineText.length && lineText[end] !== ")") {
+        end++;
+    }
+    if (end === lineText.length + 1) {
+        void vscode.window.showErrorMessage("No link found");
+        return;
+    }
+    return lineText.substring(start + 1, end);
 }
 
 /** indicates whether the given */
 export function isWebLink(text: string): boolean {
-  return text.startsWith("https://") || text.startsWith("http://")
+    return text.startsWith("https://") || text.startsWith("http://");
 }
