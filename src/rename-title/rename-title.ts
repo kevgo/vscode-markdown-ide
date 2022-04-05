13cbbbf422670a5e841d97de070b9985957d3307
Kevin Goslar <kevin.goslar@gmail.com>
2022-04-04 21:24:55 -0500

    Update doc title when renaming title (#80)

--- src/rename-title/rename-title.ts
+++ src/rename-title/rename-title.ts
@@ -2,20 +2,20 @@ import { promises as fs } from "fs"
 import * as path from "path"
 import * as vscode from "vscode"
 
-import * as line from "./helpers/line"
-import * as links from "./helpers/links"
+import * as line from "../helpers/line"
+import * as links from "../helpers/links"
 
 export async function renameTitle(): Promise<void> {
   // flush all open changes to the filesystem since we are reading files below
   await vscode.workspace.saveAll(false)
 
   const editor = vscode.window.activeTextEditor
-  if (editor === undefined) {
+  if (!editor) {
     // no open editor
     return
   }
   const activeFilePath = editor.document.fileName
-  if (activeFilePath === undefined) {
+  if (!activeFilePath) {
     // active file has no name
     return
   }
@@ -28,7 +28,7 @@ export async function renameTitle(): Promise<void> {
   }
   const oldTitle = line.removeLeadingPounds(titleLine.text)
   const newTitle = await enterTitle(oldTitle)
-  if (newTitle === undefined) {
+  if (!newTitle) {
     // user aborted the dialog
     return
   }
@@ -40,8 +40,18 @@ export async function renameTitle(): Promise<void> {
   await vscode.window.withProgress(
     { location: vscode.ProgressLocation.Window, title: "updating link titles", cancellable: false },
     async () => {
-      // replace the old title in all documents in the current workspace
-      const edit = new vscode.WorkspaceEdit()
+      const edit = new vscode.WorkspaceEdit() // change the title of the current document
+
+      // update the title in the active document
+      const doc = vscode.window.activeTextEditor?.document
+      if (!doc) {
+        return
+      }
+      const newText = changeTitle({ eol: eol2string(doc.eol), newTitle, oldTitle, text: doc.getText() })
+      const range = new vscode.Range(0, 0, doc.lineCount, 0)
+      edit.replace(doc.uri, range, newText)
+
+      // update the title of all affected links in all documents
       for (const file of await vscode.workspace.findFiles("**/*.md")) {
         const pathToActive = path.relative(path.dirname(file.fsPath), activeFilePath)
         const oldContent = await fs.readFile(file.fsPath, "utf8")
@@ -49,15 +59,23 @@ export async function renameTitle(): Promise<void> {
         if (newContent === oldContent) {
           continue
         }
-        const range = new vscode.Range(
-          new vscode.Position(0, 0),
-          new vscode.Position(line.count(oldContent), 0)
-        )
+        const range = new vscode.Range(0, 0, line.count(oldContent), 0)
         edit.replace(file, range, newContent)
       }
       await vscode.workspace.applyEdit(edit)
     }
   )
+}
+
+export function changeTitle(args: { eol: string; newTitle: string; oldTitle: string; text: string }): string {
+  const lines = args.text.split(args.eol)
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].startsWith(`# ${args.oldTitle}`)) {
+      lines[i] = `# ${args.newTitle}`
+      break
+    }
+  }
+  return lines.join(args.eol)
 }
 
 /** lets the user enter the new document title via a text input dialog */
@@ -67,4 +85,15 @@ async function enterTitle(oldTitle: string): Promise<string | undefined> {
     value: oldTitle,
     valueSelection: [oldTitle.length, oldTitle.length]
   })
+}
+
+function eol2string(eol: vscode.EndOfLine): string {
+  switch (eol) {
+    case vscode.EndOfLine.LF:
+      return "\n"
+    case vscode.EndOfLine.CRLF:
+      return "\r\n"
+    default:
+      throw new Error(`Unknown EndOfLine: ${eol}`)
+  }
 }
