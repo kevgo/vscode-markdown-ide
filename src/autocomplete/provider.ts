666664d25ba726e2d62521f2e876ef3d75ce82b0
Kevin Goslar <kevin.goslar@gmail.com>
2022-05-15 14:35:50 -0500

    Fix capitalization (#107)

--- src/autocomplete/provider.ts
+++ src/autocomplete/provider.ts
@@ -30,7 +30,7 @@ export function createCompletionProvider(
         case AutocompleteType.IMG:
           return imgCompletionItems({ debug, documentDir, startTime, wsRoot: workspacePath })
         case AutocompleteType.HEADING:
-          return HeadingCompletionItems({ debug, documentDir, startTime, wsRoot: workspacePath })
+          return headingCompletionItems({ debug, documentDir, startTime, wsRoot: workspacePath })
       }
     }
   }
@@ -71,7 +71,7 @@ export function determineType(line: string, pos: number): AutocompleteType {
   return AutocompleteType.MD_LINK
 }
 
-async function HeadingCompletionItems(
+async function headingCompletionItems(
   args: {
     debug: vscode.OutputChannel
     documentDir: string
