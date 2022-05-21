da0ac83081dfb45f788b6cd2444ef6892ecc2410
Kevin Goslar <kevin.goslar@gmail.com>
2022-05-21 10:33:52 -0500

    tikibase-provider.test.ts

--- src/code-action/tikibase-provider.test.ts
+++ src/code-action/tikibase-provider.test.ts
@@ -3,14 +3,10 @@ import { strict as assert } from "assert"
 import { mdFileName } from "./tikibase-provider"
 
 suite("mdFileName", function() {
-  const tests = {
-    "one two": "one-two.md",
-    "": ""
-  }
-  for (const [give, want] of Object.entries(tests)) {
-    test(`"${give}" --> "${want}"`, function() {
-      const have = mdFileName(give)
-      assert.equal(have, want)
-    })
-  }
+  test("normal", function() {
+    const give = "one two"
+    const want = "one-two.md"
+    const have = mdFileName(give)
+    assert.equal(have, want)
+  })
 })
