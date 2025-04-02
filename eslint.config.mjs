577a917fd0811b465f5c49068b66bf97f57ec001
Kevin Goslar <kevin.goslar@gmail.com>
2025-04-02 14:05:06 -0500

    Update ESLint (#141)

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+
+import tslintPlugin from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+
+export default [
+  {
+    files: ["src/*.ts"],
+    ignores: ["**/node_modules/", ".git/", "dist/", "out/"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: {
+        console: "readonly",
+        module: "readonly",
+        process: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tslintPlugin,
+    },
+    rules: {
+      ...tslintPlugin.configs.recommended.rules,
+      "no-empty-function": "error",
+      "prefer-const": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-call": "error",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+]
