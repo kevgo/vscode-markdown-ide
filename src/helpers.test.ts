121d4521cf222faad079885867ea3ee179ca8d54
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-01 08:55:14 -0500

    helpers.test.ts

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -3,7 +3,7 @@ import { strict as assert } from "assert"
 import * as strings from "./helpers"
 
 suite("strings", function() {
-  suite("changeTitle", function() {
+  suite("changeMdTitle", function() {
     test("normal", function() {
       const give = `# old title\ntext\n### section 1\nmore text`
       const want = `# new title\ntext\n### section 1\nmore text`
