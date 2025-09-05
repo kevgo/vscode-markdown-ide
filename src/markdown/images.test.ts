503d4f58f2dbee2b37d9e496d7d05e8660aaf3bc
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-05 17:33:31 -0500

    images.test.ts

--- src/markdown/images.test.ts
+++ src/markdown/images.test.ts
@@ -2,9 +2,11 @@ import { strict as assert } from "assert"
 import * as images from "./images"
 
 suite("image", function() {
-  test("valid filename", function() {
-    const have = images.create("foo.png")
-    const want = "[](foo.png)"
-    assert.equal(have, want)
+  suite("create", function() {
+    test("valid filename", function() {
+      const have = images.create("foo.png")
+      const want = "[](foo.png)"
+      assert.equal(have, want)
+    })
   })
 })
